a9630de4975bf1b2470953f3053c552176a9a023
cherylwang298 <cherylwang22@gmail.com>
2026-06-26 02:32:35 +0700

    fix login

--- resources/views/dummy_pages/partials/navbar.blade.php
+++ resources/views/dummy_pages/partials/navbar.blade.php
@@ -9,20 +9,52 @@
         <!-- MENU -->
         <div class="hidden md:flex gap-6 text-sm font-medium text-slate-600">
             <a href="#" class="hover:text-blue-600">Home</a>
-            <a href="#" class="hover:text-blue-600">Hotels</a>
-            <a href="#" class="hover:text-blue-600">Villas</a>
-            <a href="#" class="hover:text-blue-600">Promo</a>
+            <a href="#" class="hover:text-blue-600">Accomodation</a>
+            <a href="#" class="hover:text-blue-600">Flights</a>
+            <a href="#" class="hover:text-blue-600">Deals</a>
+            <a href="#" class="hover:text-blue-600">My Bookings</a>
         </div>
 
         <!-- AUTH -->
         <div class="flex gap-3">
-            <a href="#" class="text-sm px-4 py-2 rounded-full border hover:bg-slate-50">
-                Login
-            </a>
-            <a href="#" class="text-sm px-4 py-2 rounded-full bg-blue-600 text-white hover:bg-blue-700">
-                Sign Up
-            </a>
-        </div>
+
+            {{-- {{ Auth::check() ? 'LOGIN' : 'GUEST' }} --}}
+
+    @auth
+
+        <span class="text-sm px-4 py-2">
+            Hi, {{ Auth::user()->first_name }}
+        </span>
+
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button
+                type="submit"
+                class="text-sm px-4 py-2 rounded-full border hover:bg-slate-50"
+            >
+                Logout
+            </button>
+        </form>
+
+    @else
+
+        <a
+            href="{{ route('login.form') }}"
+            class="text-sm px-4 py-2 rounded-full border hover:bg-slate-50"
+        >
+            Login
+        </a>
+
+        <a
+            href="{{ route('register.form') }}"
+            class="text-sm px-4 py-2 rounded-full bg-blue-600 text-white hover:bg-blue-700"
+        >
+            Sign Up
+        </a>
+
+    @endauth
+
+</div>
 
     </div>
 </nav>
